71c1aabe3ef2a2f0c8cc879323b5e3087cb9aec8
Shreyansh Raj <shreyanshgupt12@gmail.com>
2025-02-12 19:20:00 +0530

    "working Perfectly"

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,101 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useEffect, useState } from 'react';
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+
+const [isStart, setStart] = useState(false);
+const [hours, setHours] = useState(0);
+const [minutes, setMinutes] = useState(0);
+const [seconds, setSeconds] = useState(0);
+const [timerid, setTimerId] = useState(0);
+
+const handleStart = ()=>{
+  setStart(true);
+}
+const handleReset = ()=>{
+  setStart(false);
+}
+
+const handleInput = (e)=>{
+const value = parseInt(e.target.value);
+const id = e.target.id;
+if(id == 'hours'){
+  setHours(value);
+}
+else if(id == 'minute'){
+  setMinutes(value)
+}
+else{
+  setSeconds(value);
+}
+}
+
+const runTimer = (sec,min, hr, tid) =>{
+  if(sec > 0){
+    setSeconds((s)=> s-1);
+  }
+  else if(sec === 0 && min > 0){
+    setMinutes((m)=> m-1);
+    setSeconds(59);
+  }
+  else if (hr > 0 ){
+    setHours((h)=> h-1);
+    setMinutes(59);
+    setSeconds(59);
+  }
+  else{
+    setStart(false);
+  //  clearInterval(tid);
+  }
+}
+
+
+
+useEffect(()=>{
+let tid;
+if(isStart){
+  tid = setInterval(()=>{
+    runTimer(seconds,minutes,hours,tid)
+  }, 1000)
+setTimerId(tid)
+}
+return ()=>{
+  clearInterval(tid);
+}
+},[isStart, hours,minutes,seconds])
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+   <div className='App'>
+      <h1>Countdown Timer</h1>
+     {!isStart &&  <div className='input-container'>
+        <div className='input-box'>
+          <input onChange={handleInput} id="hours" placeholder='HH' />
+          <input onChange={handleInput} id="minute" placeholder="MM"/>
+          <input onChange={handleInput} id="second" placeholder="SS"/>
+       
+        </div>
+        <button className='btn-start' onClick={handleStart}>Start</button>
+      </div>}
+
+    {
+    isStart &&   <div>
+        <div>
+        <div>{hours}</div>
+        <span>:</span>
+        <div>{minutes}</div>
+        <span>:</span>
+        <div>{seconds}</div>
+        </div>
+        <div>
+          <button>Pause</button>
+          <button onClick={handleReset}>Reset</button>
+        </div>
+      </div>}
+   </div>
   )
 }
 
